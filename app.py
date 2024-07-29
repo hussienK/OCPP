99355e9b2d4cd26f913da0cc1c93d66041941952
hussienK <hussienkenaan93@gmail.com>
2024-07-29 20:02:49 +0300

    new

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 import asyncio
 import os
 import websockets
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Get the port from environment variables (default to 8765 if not set)
-PORT = int(os.getenv("PORT", 8765))
 
 async def handle_message(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
 
-start_server = websockets.serve(handle_message, "0.0.0.0", PORT)
+start_server = websockets.serve(handle_message, "0.0.0.0", 9000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
-print(f"WebSocket server started on ws://0.0.0.0:{PORT}")
+print(f"WebSocket server started on ws://0.0.0.0: 9000")
 asyncio.get_event_loop().run_forever()
