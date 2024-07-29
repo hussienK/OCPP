61c561fdd83d5f9b4afe1095f74e9c03f7db2160
hussienK <hussienkenaan93@gmail.com>
2024-07-29 14:25:32 +0300

    initial commit - for deployment testing

---
+++ logger.py
@@ -0,0 +1,18 @@
+import logging
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        log_message = super().format(record)
+        return f"{log_message}\n\n"
+
+def create_logger():
+	# Create a custom handler and formatter
+	handler = logging.StreamHandler()
+	formatter = CustomFormatter('%(levelname)s: %(message)s')
+	handler.setFormatter(formatter)
+
+	# Set up the root logger
+	logger = logging.getLogger()
+	logger.setLevel(logging.INFO)
+	logger.handlers.clear()  # Remove any existing handlers
+	logger.addHandler(handler)
---
+++ utils.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from supabase import Client, create_client
+import time
+import random
+
+def	print_spaced(a):
+	print('\n\n\n')
+	print(a)
+	print('\n\n\n')
+
+# User creation function
+def create_user(id_tag, name, expiry_duration_days):
+	DB_URL = "https://gjiuhpvnfbpjjjglgzib.supabase.co"
+	DB_API = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqaXVocHZuZmJwampqZ2xnemliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwMDg5NDEsImV4cCI6MjAzNzU4NDk0MX0.B2CDr48yxglPKG6uEfAt9OPj2K-ZmqVHSeW6Bb_SW70"
+	supabase: Client = create_client(DB_URL, DB_API)
+	expiry_date = (datetime.now() + timedelta(days=expiry_duration_days)).isoformat()
+	user_data = {
+		'id_tag': id_tag,
+		'name': name,
+		'expiry_date': expiry_date
+	}
+	response = supabase.table('users').insert(user_data).execute()
+	return response
+
+def create_charge_point(location, status, manuf, firmware_version, meter_reading):
+	DB_URL = "https://gjiuhpvnfbpjjjglgzib.supabase.co"
+	DB_API = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqaXVocHZuZmJwampqZ2xnemliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIwMDg5NDEsImV4cCI6MjAzNzU4NDk0MX0.B2CDr48yxglPKG6uEfAt9OPj2K-ZmqVHSeW6Bb_SW70"
+	supabase: Client = create_client(DB_URL, DB_API)
+	charge_point_data = {
+		'location': location,
+		'status': status,
+		'manufacturer': manuf,
+		'firmware_version': firmware_version,
+		'meter_reading': meter_reading,
+	}
+	response = supabase.table('charge_points').insert(charge_point_data).execute()
+	return response
+
+def generate_transaction_id():
+	timestamp = int(time.time() * 1000)
+	random_numb = random.randint(1000, 9999)
+	return abs(timestamp - random_numb)
