9279f870fb7772bfe9182f59799da1dbbadf861b
hussienK <hussienkenaan93@gmail.com>
2024-07-29 20:15:36 +0300

    new

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ import asyncio
 import os
 import websockets
 
-# Get the port from environment variables (default to 8765 if not set)
-
 async def handle_message(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
+        await websocket.send(f"Echo: {message}")
 
-start_server = websockets.serve(handle_message, "0.0.0.0", 9000)
+async def start_server():
+    print(f"Starting server on port 9000")
+    try:
+        server = await websockets.serve(handle_message, "0.0.0.0", 9000)
+        print(f"Server started on ws://0.0.0.0: 9000")
+        await server.wait_closed()
+    except Exception as e:
+        print(f"Failed to start server: {e}")
 
-asyncio.get_event_loop().run_until_complete(start_server)
-print(f"WebSocket server started on ws://0.0.0.0: 9000")
+asyncio.get_event_loop().run_until_complete(start_server())
 asyncio.get_event_loop().run_forever()
